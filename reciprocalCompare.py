d2ab197d4a9d558a019794267097eda3f9ccfe67
Matthew Soulanille <matthew@flute.caltech.edu>
2014-06-05 16:39:42 -0700

    Restructured the interperatation of flags. Descovered that e value sorting is wrong. It assumes things like '1e-4' are strings instead of floats. This is not yet fixed (I am out of time right now). Added some error messages. It is now possible to output to stdout.

--- reciprocalCompare.py
+++ reciprocalCompare.py
@@ -1,9 +1,10 @@
+# Fix sorting e values like they are words
 import sys, csv
+from functools import partial
 e_cutoff = 1e-3
 
 def printHelp():
-    print("This is the help text. Helpful, right? :P will change.")
-
+    sys.exit("This is the help text. Helpful, right? :P will change.")
 
 def readArgs():
     file1 = ''
@@ -20,7 +21,7 @@ def readArgs():
         elif output == '':
             output = each
         else:
-            raise Exception("Too many arguments (2 files and 1 output max).")
+            sys.exit("Error: Too many arguments (2 files and 1 output max).")
     return [arguments, file1, file2, output]
             
 args = readArgs()
@@ -29,30 +30,51 @@ def list_line_sort(data, line):
     data.sort(key=lambda x: x[line])
 
 line = 2
+outputMode = 0
+prefdict = {"outputMode":0, "sort":2}
 
-for flag in args[0]:
-    if args[0] == []:
-        break
-    if flag[0] == flag[1] == '-':
-        if flag == '--help':
-            printHelp()
-        elif flag == '--sort=e':
-            line = 2
-        elif flag == '--sort=file1':
-            line = 0
-        elif flag == '--sort=file2':
-            line = 1
-        else:
-            raise Exception("Unrecognized argument: " + flag + " for help, run with --help")
-        break
-    elif 'e' in flag:
-        line = 2
-    elif '1' in flag:
-        line = 0
-    elif '2' in flag:
-        line = 1
+def assign(key, value, dictionary):
+    dictionary[key] = value
+def passign(key, value):
+    assign(key, value, prefdict)
+
+
+flagdict = {"e":partial(passign, "sort", 2), "--sort=e":partial(passign, "sort", 2), "1":partial(passign, "sort", 0), "--sort=file1":partial(passign, "sort", 0), "2":partial(passign, "sort", 1), "--sort=file2":partial(passign, "sort", 1), "--stdout":partial(passign, "outputMode", 1), "s":partial(passign, "outputMode", 1)}
+
+def checkDefined(l, b, e):
+    returnlist = []
+    for x in range(b, e+1):
+        try: l[x]
+        except: return False
+        if l[x] == '':
+            return False
+    return True
+
+for arg in args[0]:
+    if arg[0] == arg[1] == '-':
+        try: flagdict[arg]()
+        except: sys.exit("Error: Unknown argument '" + str(arg) + "' Use --help for help.")
+    elif arg[0] == '-':
+        for letter in arg[1:]:
+            try: flagdict[letter]()
+            except: sys.exit("Error: Unknown argument '" + str(letter) + "' Use --help for help.")
     else:
-        raise Exception("Unrecognized argument: " + flag + " for help, run with --help")
+        raise Exception("something is very broken D:")
+        
+        
+                
+
+if prefdict["outputMode"] == 0: # Output in file mode
+    if checkDefined(args, 1, 3) == False:
+        sys.exit("Error: Not enough arguments. Requires two files to compare and a name for an output file.")
+elif prefdict["outputMode"] == 1: # Output in stdout mode
+    if checkDefined(args, 3, 3) == True:
+        sys.exit("Error: Too many arguments. If using stdout as output, no output file name should be given")
+    elif checkDefined(args, 1, 2) == False:
+        sys.exit("Error: Too few arguments. Two files are required for comparison. No output file when using stdout as output")
+
+
+
 
 
 
@@ -83,8 +105,10 @@ for iterate in xrange(0, min(len(d1), len(d2))-1):
         output.append(outlist)
 
 
-list_line_sort(output, line)        
-
-with open(args[3], 'w+') as outfile:
-    for line in output:
-        outfile.write(str(line) + '\n')
+list_line_sort(output, prefdict["sort"])
+if prefdict["outputMode"] == 0:
+    with open(args[3], 'w+') as outfile:
+        for line in output:
+            outfile.write(str(line) + '\n')
+elif prefdict["outputMode"] == 1:
+    sys.stdout.write(str(output))
